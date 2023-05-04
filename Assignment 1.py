9b315878d00a5be5d40f1f29e4397adcae18dbc7
Dineshbabu26 <98269099+Dineshbabu26@users.noreply.github.com>
2023-05-04 13:53:55 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,16 @@
+# Add the digits
+num=int(input("Enter the digit"))
+n=0
+for i in str(num):
+    n+=int(i)
+print(n)
+
+#SWAP
+
+a=10
+b=20
+print("Before Swapping ",a,b)
+t=a
+a=b
+b=t
+print("After  Swapping ",a,b)
